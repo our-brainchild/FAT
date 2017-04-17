366b103c1177e8d5c4243104b03e0be9636d8132
AntonKryachko <akruachko@gmail.com>
2017-04-18 00:45:01 +0300

    FAT-7 Создание БО Студент (завершено)

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,20 @@
 package controllers;
 
+import DAO.StudentService;
+import DAO.StudentServiceImpl;
+import model.Student;
+import model.utils.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import play.api.db.Database;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.io.File;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -18,6 +30,10 @@ public class HomeController extends Controller {
      * some comment
      */
     public Result index() {
+
+        Student student = new Student("Anton", "ИВТ-32о", 2840.0, "09.03.01");
+        StudentService studentService = new StudentServiceImpl();
+        studentService.remove(9);
         return ok(index.render("Your new application is ready."));
     }
 
